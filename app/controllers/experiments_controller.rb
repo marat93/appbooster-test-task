0850e9ef4823afb6fe1019e868043419593debe3
Marat Mamytov <e66m@ya.ru>
2023-06-07 15:36:56 +0600

    Implement logic for assigning experiments with devices

---
+++ app/controllers/experiments_controller.rb
@@ -0,0 +1,17 @@
+class ExperimentsController < ApplicationController
+    def index
+        if device_id.blank?
+            return render json: { error: { message: 'Device-Token header is empty' } }, status: :bad_request
+        end
+
+        device = ExperimentsAssigner.new(device_id).call
+
+        render json: device
+    end
+
+    private
+
+    def device_id
+        request.headers['Device-Token']
+    end
+end
