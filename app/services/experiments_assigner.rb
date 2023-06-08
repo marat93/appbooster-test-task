aca1d017a67bd98f1fa4cd262a0904f825cd7944
Marat Mamytov <e66m@ya.ru>
2023-06-08 14:27:24 +0600

    Refactor experiment assiging option

--- app/services/experiments_assigner.rb
+++ app/services/experiments_assigner.rb
@@ -4,29 +4,33 @@ class ExperimentsAssigner
   end
 
   def call
-    experiments = Rails.application.config_for(:experiments).with_indifferent_access
-    experiments.each do |experiment, values|
-      $redis.hset(experiment, values) if $redis.exists(experiment).zero?
+    Device.find_or_create_by!(device_id: @device_id) do |device|
+      assign_experiments(device)
     end
-
-    device = Device.find_or_create_by!(device_id: @device_id)
-
-    device_experiment_names = device.assigned_experiments.map(&:experiment_name)
-    new_assigned_experiments = []
-
-    experiments.keys.difference(device_experiment_names).each do |experiment|
-      current_values = $redis.hgetall(experiment)
-      highest_key = arrange_percentages!(current_values, experiments[experiment])
-      $redis.hset(experiment, current_values)
-      new_assigned_experiments << { experiment_name: experiment, experiment_option: highest_key }
-    end
-
-    device.assigned_experiments.create!(new_assigned_experiments)
-
-    device
   end
 
   private
+
+  def assign_experiments(device)
+    experiments = Rails.application.config_for(:experiments).with_indifferent_access
+
+    load_new_experiments(experiments)
+
+    experiments.each_key do |experiment|
+      current_values = $redis.hgetall(experiment)
+      highest_key = arrange_percentages!(current_values, experiments[experiment])
+      $redis.hset(experiment, current_values)
+      device.assigned_experiments.new(experiment_name: experiment, experiment_option: highest_key)
+    end
+
+    device.save!
+  end
+
+  def load_new_experiments(experiments)
+    experiments.each do |experiment, values|
+      $redis.hset(experiment, values) if $redis.exists(experiment).zero?
+    end
+  end
 
   def arrange_percentages!(current_percentages, initial_percentages)
     highest_percentage_key = current_percentages.keys.first
